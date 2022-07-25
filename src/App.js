5065f6c7807ddeed1c43b6b39425611c254eba9a
MARY <marymwihaki123@gmail.com>
2022-07-25 15:34:05 -0500

    editing app.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import Header from 'header';
-import Main from 'main';
-import logo from './logo.svg';
+import Header from './Header.js';
+import Main from './Main.js'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component{
+
+  render(){
+    return(
       <>
       <Header/>
-      
-      <Main />
-      <footer> Author: Mary Kariuki</footer>
-
-    </div>
-  );
+      <Main/>
+      <footer/>
+      </>
+    )
+  }
 }
-
-export default App;
