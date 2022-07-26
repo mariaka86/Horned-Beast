b7d698051f1aa4260aa2b16b3fd1771038b72f80
MARY <marymwihaki123@gmail.com>
2022-07-26 15:41:27 -0500

    finished first feature 2:10 to 3:36

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,32 @@
 import React from "react";
 class HornedBeast extends React.Component{
+    constructor(props){
+        super(props);
+            this.state={
+                favorites:0
+        };
+    };
+
+    handleFavorites= () => {
+       this.setState({
+        favorites: this.state.favorites + 1
+
+       });
+    };
+
+
+
+
+
+
     render(){
         console.log('props in the hornedbeast',this.props)
         return(
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.imageUrl} alt={this.props.title}/> 
+                <img src={this.props.image_url} alt={this.props.title}/> 
                  <p> {this.props.description}</p>
+                 <p>{this.state.favorites} favorites </p>
             </div>
 
         )
