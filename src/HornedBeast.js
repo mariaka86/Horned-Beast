7f78a35032c704cd36790aa687e30e6b8d36c68c
MARY <marymwihaki123@gmail.com>
2022-07-26 15:50:54 -0500

    got sorta button to work for votes 20mins

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,11 +3,13 @@ class HornedBeast extends React.Component{
     constructor(props){
         super(props);
             this.state={
+                //count favorites
                 favorites:0
         };
     };
 
     handleFavorites= () => {
+        //sets state
        this.setState({
         favorites: this.state.favorites + 1
 
@@ -27,6 +29,7 @@ class HornedBeast extends React.Component{
                 <img src={this.props.image_url} alt={this.props.title}/> 
                  <p> {this.props.description}</p>
                  <p>{this.state.favorites} favorites </p>
+                 <p onClick={this.handleFavorites}> Click on favorite beast</p>
             </div>
 
         )
