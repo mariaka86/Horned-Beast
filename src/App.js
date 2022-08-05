d2082032686bc93c0102870212f6ee9ad5f783a5
MARY <marymwihaki123@gmail.com>
2022-08-04 21:54:51 -0500

    error deploying should be fixes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 import Header from './Header.js';
 import Main from './Main.js'
 import Footer from './Footer.js'
-import SelectedBeast from './SelectedBeast.js'
+// import SelectedBeast from './SelectedBeast.js'
 import data from './data.json'
 import Modal from 'react-bootstrap/Modal'
 
